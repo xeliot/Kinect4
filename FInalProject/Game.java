f6e9565328866f189eb5c045d390cc6fb1ac1213
xeliot <daveho8888@gmail.com>
2017-05-10 22:20:46 -0700

    add some comments

--- FInalProject/Game.java
+++ FInalProject/Game.java
@@ -5,9 +5,9 @@ public class Game{
   private int playerTurn;
   
   public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    Board board = new Board();
-    board.printBoard();
+    Scanner scan = new Scanner(System.in); // Create new scanner
+    Board board = new Board(); // Create new board
+    board.printBoard(); // Initial board print
     while(board.checkWin()==-1){
       System.out.println("Player 1, please enter a column (1-7).");
       int p1 = scan.nextInt();
