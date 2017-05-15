8fa082ba1fd5eb9be1c731a39a0fa1872909b821
xeliot <daveho8888@gmail.com>
2017-05-14 22:32:11 -0700

    prompt  user for names

--- FInalProject/Game.java
+++ FInalProject/Game.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Game{
   
-  private int playerTurn;
-  private String player1Name;
-  private String player2Name;
+  //private int playerTurn;
+  private static String player1Name;
+  private static String player2Name;
   
   public static void main(String[] args){
     System.out.println("Welcome to Kinect4");
     System.out.println("Designed by Dave, Santosh, and JJ");
     Scanner scan = new Scanner(System.in); // Create new scanner
     Board board = new Board(); // Create new board
+    System.out.println("Hi Player1. Please enter your name");
+    player1Name = scan.nextLine();
+    System.out.println("Hi Player2. Please enter your name");
+    player2Name = scan.nextLine();
     board.printBoard(); // Initial board print
     while(board.checkWin()==-1){
       System.out.println(player1Name + ", please enter a column (1-7).");
