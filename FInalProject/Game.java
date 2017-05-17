be3ea355e8a1a60439b42b91390ddafb2f49ff5c
xeliot <daveho8888@gmail.com>
2017-05-16 21:05:48 -0700

    start to revitaize game class

--- FInalProject/Game.java
+++ FInalProject/Game.java
@@ -2,9 +2,47 @@ import java.util.Scanner;
 
 public class Game{
   
-  //private int playerTurn;
-  private static String player1Name;
-  private static String player2Name;
+  private String player1Name;
+  private String player2Name;
+  private Board board;
+  
+  public Game(String p1Name, String p2Name){
+    player1Name = p1Name;
+    player2Name = p2Name;
+    board = new Board();
+  }
+  
+  public static void intro(){
+    System.out.println("Welcome to Kinect4");
+    System.out.println("Designed by Dave, Santosh, and JJ");
+    System.out.println("----------------------------------");
+  }
+  
+  public void play(){
+    intro();
+    Scanner scan = new Scanner(System.in);
+    while(board.checkWin()==-1){
+      System.out.println(player1Name + ", please enter a column (1-7).");
+      int p1 = scan.nextInt();
+      // TODO: CHECK VALIDITY OF VALUE ENTERED
+      board.dropPiece(p1-1, 0);
+      board.printBoard();
+      if(board.checkWin()!=-1){
+        System.out.println(player1Name + " wins!");
+        break;
+      }
+      System.out.println(player2Name + ", please enter a column (1-7).");
+      int p2 = scan.nextInt();
+      // TODO: CHECK VALIDITY OF VALUE ENTERED
+      board.dropPiece(p2-1, 1);
+      board.printBoard();
+      if(board.checkWin()!=-1){
+        System.out.println(player2Name + " wins!");
+        break;
+      }
+    }
+    System.out.println("Thanks for playing");
+  }
   
   public static void main(String[] args){
     System.out.println("Welcome to Kinect4");
