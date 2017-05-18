51ed58c2a73a5d396fa5534417f3d377ccc60c0b
xeliot <daveho8888@gmail.com>
2017-05-17 22:14:50 -0700

    fixed final bug

--- FInalProject/Runner.java
+++ FInalProject/Runner.java
@@ -6,7 +6,7 @@ public class Runner{
     System.out.println("Player 1, please enter your name.");
     String p1Name = scan.nextLine();
     System.out.println("Player 2, please enter your name.");
-    String p2Name = scan.nextLine();    
+    String p2Name = scan.nextLine();
     Game game = new Game(p1Name, p2Name);
     game.play();
   }
